64110a4612716ca849d44684a5412d891d18de80
jiangk77 <jiangkai2019@gmail.com>
2024-08-20 20:58:33 +0800

    ASF网站批量注册用户

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-req = requests.get('https://www.snapmail.cc/emailList/jiangkai@snapmail.cc?isPrefix=True&count=1')
-email_text = json.loads(req.text)
-print(email_text)
